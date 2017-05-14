811f3feac1bc03c36eb07ed264852dae435fc7a5
mtso <mtso@tuta.io>
2017-05-13 22:08:24 -0700

    Add AuthForm component

---
+++ app/components/AuthenticationForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const AuthenticationForm = (props) => (
+  <form onSubmit={props.onSubmit} method={props.method || 'POST'}>
+    <input
+      type='text'
+      name='username'
+      placeholder={props.usernamePlaceholder || 'Username'}
+    />
+    <input
+      type='password'
+      name='password'
+      placeholder={props.passwordPlaceholder || 'Password'}
+    />
+    <input type='submit' value={props.buttonTitle || 'Submit'} />
+  </form>
+)
